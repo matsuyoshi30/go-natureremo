541539c60ce34f4ec03ed42997b62daa76256fc1
matsuyoshi <sfbgwm30@gmail.com>
2019-12-13 01:53:39 +0900

    Add json struct

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/BurntSushi/toml"
 )
@@ -19,6 +20,42 @@ type TokenConfig struct {
 const URL = "https://api.nature.global/1/devices"
 
 var config Config
+
+type Device struct {
+	DeviceCore
+	Users        []User `json:"users"`
+	NewestEvents Events `json:"newest_events"`
+}
+
+type DeviceCore struct {
+	ID                string    `json:"id"`
+	Name              string    `json:"name"`
+	TemperatureOffset int       `json:"temperature_offset"`
+	HumidityOffset    int       `json:"humidity_offset"`
+	CreatedAt         time.Time `json:"created_at"`
+	UpdatedAt         time.Time `json:"updated_at"`
+	FirmwareVersion   string    `json:"firmware_version"`
+	MacAddress        string    `json:"mac_address"`
+	SerialNumber      string    `json:"serial_number"`
+}
+
+type User struct {
+	ID        string `json:"id"`
+	Nickname  string `json:"nickname"`
+	Superuser bool   `json:"superuser"`
+}
+
+type Events struct {
+	HU SensorValue `json:"hu"`
+	IL SensorValue `json:"il"`
+	MO SensorValue `json:"mo"`
+	TE SensorValue `json:"te"`
+}
+
+type SensorValue struct {
+	Val       float64 `json:"val"`
+	CreatedAt string  `json:"created_at"`
+}
 
 func main() {
 	_, err := toml.DecodeFile("./config.toml", &config)
@@ -48,11 +85,16 @@ func main() {
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
+	var devices []Device
+	if err := json.NewDecoder(resp.Body).Decode(&devices); err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println(string(body))
+	for _, d := range devices {
+		fmt.Printf("[Temperature]  %f°C\n", d.NewestEvents.TE.Val)
+		fmt.Printf("[Humidity]     %f%%\n", d.NewestEvents.HU.Val)
+		fmt.Printf("[Illumination] %f\n", d.NewestEvents.IL.Val)
+		fmt.Printf("[movement]     %f\n", d.NewestEvents.MO.Val)
+	}
 }
